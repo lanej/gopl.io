b96bf29533f518d825a14a43c4bd7e2a9d74dedc
Joshua Lane <me@joshualane.com>
2017-09-14 20:52:59 -0700

    ex 2.3
    Handy builtin functions

---
+++ ch2/2.3/main.go
@@ -0,0 +1,26 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 45.
+package main
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+	var count byte
+
+	for i, v := uint64(0), uint64(8); i < v; i++ {
+		count += byte(x >> (i * 8))
+	}
+	return int(count)
+}
+
+//!-
